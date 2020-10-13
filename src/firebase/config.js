fd7fc7dd1d5fce08d4cd15765dde4a7256268f5f
Pedro Trigo Miranda <pedrotrigomiranda@Pedros-MBP-2.lan>
2020-10-14 00:14:45 +0100

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIIHODGj7EwJQPcz1Q5D4XYykSFE1sELw",
+  authDomain: "hunt-c91cd.firebaseapp.com",
+  databaseURL: "https://hunt-c91cd.firebaseio.com",
+  projectId: "hunt-c91cd",
+  storageBucket: "hunt-c91cd.appspot.com",
+  messagingSenderId: "973243648269",
+  appId: "1:973243648269:web:403b658e8e18d1b8f24844",
+};
+
+export default firebaseConfig;
